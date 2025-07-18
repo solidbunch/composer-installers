048ab3efd46d6259867c73a3965803cd0bec07fc
Yurii Pavlov <mailyurik@gmail.com>
2025-07-18 18:12:20 +0100

    fix installation path

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,18 +5,17 @@ namespace SolidBunch\ComposerInstallers;
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
-use Composer\Plugin\Capable;
+use Composer\Installer\InstallationManager;
 
-class Plugin implements PluginInterface, Capable
+class Plugin implements PluginInterface
 {
-    public function activate(Composer $composer, IOInterface $io) {}
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $installationManager = $composer->getInstallationManager();
+        $installer = new Installer($io, $composer);
+        $installationManager->addInstaller($installer);
+    }
+
     public function deactivate(Composer $composer, IOInterface $io) {}
     public function uninstall(Composer $composer, IOInterface $io) {}
-
-    public function getCapabilities()
-    {
-        return [
-            \Composer\Plugin\Capability\InstallerCapability::class => Installer::class,
-        ];
-    }
 }
