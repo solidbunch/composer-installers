81a139909a11bf2f2f378e4ea2469d7452be658d
Yurii Pavlov <mailyurik@gmail.com>
2025-07-18 20:08:56 +0100

    update installer - add installer-paths processing

--- src/Installer.php
+++ src/Installer.php
@@ -2,19 +2,72 @@
 
 namespace SolidBunch\ComposerInstallers;
 
-use Composer\Composer;
+use Composer\Installer\LibraryInstaller;
 use Composer\IO\IOInterface;
-use Composer\Installers\Installer as BaseInstaller;
+use Composer\Composer;
+use Composer\Package\PackageInterface;
+use Composer\Package\RootPackageInterface;
 
-class Installer extends BaseInstaller
+/**
+ * Custom installer that supports any package types defined in "extra.installer-paths" of the root project.
+ */
+class Installer extends LibraryInstaller
 {
+    protected array $paths = [];
+
+    /**
+     * Constructor that loads installer-paths from root composer.json.
+     */
     public function __construct(IOInterface $io, Composer $composer)
     {
-        parent::__construct($io, $composer);
+        // Constructor compatible with Composer 2.2+
+        parent::__construct($io, $composer, 'library', null);
 
-        $this->locations = array_merge($this->locations, [
-            'kit-module'     => 'kit-modules/{$name}/',
-            'wordpress-core' => 'cweb/wp-core/',
-        ]);
+        $rootPackage = $composer->getPackage();
+
+        // Make sure we get installer-paths from the root composer.json
+        if ($rootPackage instanceof RootPackageInterface) {
+            $this->paths = $rootPackage->getExtra()['installer-paths'] ?? [];
+        }
+    }
+
+    /**
+     * Returns the custom install path for the given package.
+     */
+    public function getInstallPath(PackageInterface $package): string
+    {
+        $type = $package->getType();
+        $name = $package->getPrettyName(); // vendor/name
+
+        foreach ($this->paths as $path => $criteriaList) {
+            foreach ($criteriaList as $criteria) {
+                if (str_starts_with($criteria, 'type:')) {
+                    if ($type === substr($criteria, 5)) {
+                        return $this->replaceVars($path, $name);
+                    }
+                } elseif ($criteria === $name) {
+                    return $this->replaceVars($path, $name);
+                }
+            }
+        }
+
+        return parent::getInstallPath($package);
+    }
+
+    /**
+     * Indicates that this installer supports any package type.
+     */
+    public function supports(string $packageType): bool
+    {
+        return true; // supports all types (custom or default)
+    }
+
+    /**
+     * Replaces placeholders like {$vendor} and {$name} in the target path.
+     */
+    private function replaceVars(string $path, string $prettyName): string
+    {
+        [$vendor, $name] = explode('/', $prettyName, 2);
+        return str_replace(['{$vendor}', '{$name}'], [$vendor, $name], $path);
     }
 }
--- src/Plugin.php
+++ src/Plugin.php
@@ -6,14 +6,33 @@ use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
+/**
+ * Composer plugin that registers the custom installer.
+ */
 class Plugin implements PluginInterface
 {
-    public function activate(Composer $composer, IOInterface $io)
+    /**
+     * Called when the plugin is activated.
+     */
+    public function activate(Composer $composer, IOInterface $io): void
     {
         $installer = new Installer($io, $composer);
         $composer->getInstallationManager()->addInstaller($installer);
     }
 
-    public function deactivate(Composer $composer, IOInterface $io) {}
-    public function uninstall(Composer $composer, IOInterface $io) {}
+    /**
+     * Called when the plugin is deactivated (Composer 2+).
+     */
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+        // Optional: no action needed
+    }
+
+    /**
+     * Called when the plugin is uninstalled (Composer 2+).
+     */
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+        // Optional: no action needed
+    }
 }
